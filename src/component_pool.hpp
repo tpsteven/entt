2244e03a2cb1b0aab67cd6d562bad7ecae21fb80
Tyler Stevens <tpsteven@uci.edu>
2017-05-28 15:10:49 -0700

    Initial ComponentPool documentation

--- src/component_pool.hpp
+++ src/component_pool.hpp
@@ -13,7 +13,7 @@ namespace entt {
 template<typename, typename, typename...>
 class ComponentPool;
 
-
+// Single-component pool
 template<typename Entity, typename Component>
 class ComponentPool<Entity, Component> {
 public:
@@ -41,35 +41,43 @@ public:
 
     ComponentPool & operator=(ComponentPool &&) = default;
 
+    // Returns whether the pool is empty
     bool empty() const noexcept {
         return data.empty();
     }
 
+    // Returns the maximum number of components created until now
     size_type capacity() const noexcept {
         return data.capacity();
     }
 
+    // Returns the current number of components
     size_type size() const noexcept {
         return data.size();
     }
 
+    // Returns a direct pointer to the internal array of the Component Index -> Entity vector
     const entity_type * entities() const noexcept {
         return direct.data();
     }
 
+    // Returns whether the pool contains a component associated with the given entity
     bool has(entity_type entity) const noexcept {
         return valid(entity);
     }
 
+    // Returns a const reference to the component associated with the given entity
     const component_type & get(entity_type entity) const noexcept {
         assert(valid(entity));
         return data[reverse[entity]];
     }
 
+    // Returns a reference to the component associated with the given entity
     component_type & get(entity_type entity) noexcept {
         return const_cast<component_type &>(const_cast<const ComponentPool *>(this)->get(entity));
     }
 
+    // Constructs a component and associates it with the given entity
     template<typename... Args>
     component_type & construct(entity_type entity, Args... args) {
         assert(!valid(entity));
@@ -85,6 +93,7 @@ public:
         return data.back();
     }
 
+    // Removes the component associated with the given entity
     void destroy(entity_type entity) {
         assert(valid(entity));
 
@@ -98,6 +107,7 @@ public:
         data.pop_back();
     }
 
+    // Removes all components and entity-component associations
     void reset() {
         data.clear();
         reverse.resize(0);
@@ -105,12 +115,12 @@ public:
     }
 
 private:
-    std::vector<component_type> data;
-    std::vector<pos_type> reverse;
-    std::vector<entity_type> direct;
+    std::vector<component_type> data;   // Component storage
+    std::vector<pos_type> reverse;      // Entity -> Component Index map
+    std::vector<entity_type> direct;    // Component Index -> Entity map
 };
 
-
+// Multi-component pool
 template<typename Entity, typename Component, typename... Components>
 class ComponentPool
         : ComponentPool<Entity, Component>, ComponentPool<Entity, Components>...
@@ -139,56 +149,67 @@ public:
     ComponentPool & operator=(const ComponentPool &) = delete;
     ComponentPool & operator=(ComponentPool &&) = delete;
 
+    // Returns whether the pool (of the templated component type) is empty
     template<typename Comp>
     bool empty() const noexcept {
         return Pool<Comp>::empty();
     }
 
+    // Returns the maximum number of components (of the templated component type) created until now
     template<typename Comp>
     size_type capacity() const noexcept {
         return Pool<Comp>::capacity();
     }
 
+    // Returns the current number of components (of the templated component type)
     template<typename Comp>
     size_type size() const noexcept {
         return Pool<Comp>::size();
     }
 
+    // Returns a direct pointer to the internal array of the Component Index (of the templated component type) -> Entity vector
     template<typename Comp>
     const entity_type * entities() const noexcept {
         return Pool<Comp>::entities();
     }
 
+    // Returns whether the pool contains a component (of the templated component type) associated with the given entity
     template<typename Comp>
     bool has(entity_type entity) const noexcept {
         return Pool<Comp>::has(entity);
     }
 
+    // Returns a const reference to the component (of the templated component type) associated with the given entity
     template<typename Comp>
     const Comp & get(entity_type entity) const noexcept {
         return Pool<Comp>::get(entity);
     }
 
+    // Returns a const reference to the component (of the templated component type) associated with the given entity
     template<typename Comp>
     Comp & get(entity_type entity) noexcept {
         return const_cast<Comp &>(const_cast<const ComponentPool *>(this)->get<Comp>(entity));
     }
 
+    // Constructs a component (of the templated component type) and associates it with the given entity
     template<typename Comp, typename... Args>
     Comp & construct(entity_type entity, Args... args) {
         return Pool<Comp>::construct(entity, args...);
     }
 
+    // Removes the component (of the templated component type) associated with the given entity
     template<typename Comp>
     void destroy(entity_type entity) {
         Pool<Comp>::destroy(entity);
     }
 
+    // Removes all components and entity-component associations (of the templated component type)
     template<typename Comp>
     void reset() {
         Pool<Comp>::reset();
     }
 
+    // Removes all components and entity-component associations (for all component types stored in pool)
     void reset() {
         using accumulator_type = int[];
         Pool<Component>::reset();
